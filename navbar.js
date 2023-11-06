1c42d88ddc69f95950c58c314fc0270fbf980f06
espenraugstad <espen.raugstad@gmail.com>
2023-11-06 22:16:58 +0100

    Added a small example with a nav-bar custom component

---
+++ navbar.js
@@ -0,0 +1,52 @@
+class NavBar extends HTMLElement {
+    constructor(){
+        super();
+    }
+
+    connectedCallback(){
+        console.log("Connected");
+        /* Style */
+        const styleSheet = new CSSStyleSheet();
+        styleSheet.replaceSync(`
+        nav{
+            width: 100%
+        } 
+        ul {
+            width: 100%; 
+            list-style: none; 
+            display: flex; 
+            justify-content: flex-end;
+        } 
+        li {
+            margin: 0 1rem;
+            font-size: 1.10rem;
+        }
+        a {
+            text-decoration: none;
+            color: black;
+        }
+        a:hover{
+            text-decoration: underline;
+        }
+        
+        `);
+
+        /* Shadow DOM */
+        const shadowRoot = this.attachShadow({mode: "open"});
+        shadowRoot.adoptedStyleSheets = [styleSheet];
+        const nav = document.createElement("nav");
+        nav.setAttribute("part", "nav-bar");
+        nav.innerHTML = `
+        <ul>
+            <li><a href="index.html">Home</a></li>
+            <li><a href="services.html">Services</a></li>
+            <li><a href="about.html">About</a></li>
+            <li><a href="contact.html">Contact</a></li>
+        </ul>
+        `
+        shadowRoot.appendChild(nav);
+
+    }
+}
+
+customElements.define("nav-bar", NavBar);
